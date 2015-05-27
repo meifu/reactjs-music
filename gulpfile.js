1dfe5c1ddec6cad5f8c56cddfd9906ac3ca503f1
meifu <meifutoolate@gmail.com>
2015-05-27 23:48:16 +0800

    why...

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,15 @@ var watchify = require('watchify');
 var reactify = require('reactify');
 
 gulp.task('browserify', function() {
-	var bundler = browserify({
-		entries: ['./public/scripts/app.js'],
-		transform: [reactify],
-		debug: true
-	});
+	// var bundler = browserify({
+	// 	entries: ['./public/scripts/app.js'],
+	// 	transform: [reactify],
+	// 	debug: true
+	// });
 
-	browserify(bundler).bundle().pipe(source('app.js')).pipe(gulp.dest('./build/scripts/'));
+	// browserify(bundler).bundle().pipe(source('app.js')).pipe(gulp.dest('./build/scripts/'));
+	var b = browserify();
+	b.transform(reactify).add('./public/scripts/app.js');
+	return b.bundle().pipe(source('app.js')).pipe(gulp.dest('./build/scripts/'));
+	
 });
