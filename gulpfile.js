fa098838ab2733ff0ca9ad4f09d1cb013d2d23a1
May's Mac <KUOM@MaysdeiMac.local>
2015-05-28 19:16:58 +0800

    add something to gulpfiles

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,58 @@
-var gulp = require('gulp');
-var source = require('vinyl-source-stream');
-var browserify = require('browserify');
-var watchify = require('watchify');
-var reactify = require('reactify');
+var gulp = require('gulp'),
+	source = require('vinyl-source-stream'),
+	browserify = require('browserify'),
+	watchify = require('watchify'),
+	reactify = require('reactify'),
+	sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    rename = require('gulp-rename');
 
 gulp.task('browserify', function() {
-	var bundler = browserify({
-		entries: ['./public/scripts/app.js'],
-		transform: [reactify],
-		debug: true
-	});
+	var b = browserify();
+	b.transform(reactify); // use the reactify transform
+	b.add('./public/scripts/app.js');
+	return b.bundle()
+		.pipe(source('app.js'))
+		.pipe(gulp.dest('./build/scripts'));
 
-	browserify(bundler).bundle().pipe(source('app.js')).pipe(gulp.dest('./build/scripts/'));
 });
+
+
+gulp.task('browserify2', function() {
+	//var bundler = browserify({
+	//	entries: './public/scripts/app.js',
+	//	transform: [reactify],
+	//	debug: true
+	//});
+
+	//browserify(bundler).bundle().pipe(source('app.js')).pipe(gulp.dest('./build/scripts/'));
+	browserify('./public/scripts/app.js').transform(reactify).bundle().pipe(source('app.js')).pipe(gulp.dest('./build/scripts/'));
+});
+
+gulp.task('express', function() {
+	var express = require('express');
+	var path = require('path');
+	var app = express();
+	app.use('/', express.static(path.join(__dirname, 'build')));
+	app.listen(4000);
+});
+
+gulp.task('styles', function() {
+	return gulp.src('scss/*.scss')
+		.pipe(sass({ style: 'expanded' }))
+		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+		.pipe(gulp.dest('./build/css/'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(minifycss())
+		.pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('sass/*.scss', ['styles']);
+	//gulp.watch('*.html', notifyLiveReload);
+	//gulp.watch('css/*.css', notifyLiveReload);
+});
+
+
+
